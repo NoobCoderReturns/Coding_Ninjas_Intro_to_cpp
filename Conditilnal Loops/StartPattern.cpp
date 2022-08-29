cb1fd7875d61bbb17dd8dcb97933d0f40f88c790
NoobCoderReturns <112306726+NoobCoderReturns@users.noreply.github.com>
2022-08-29 21:27:22 +0530

    Update StartPattern.cpp

--- Conditilnal Loops/StartPattern.cpp
+++ Conditilnal Loops/StartPattern.cpp
@@ -1,60 +1,52 @@
-Start Pattern
-Print the following pattern
-Pattern for N = 4
-    *
-   *** 
-  *****
- *******
-Input Format :
-N (Total no. of rows)
-Output Format :
-Pattern in N lines
-Constraints :
-0 <= N <= 50
-Sample Input 1 :
-3
-Sample Output 1 :
-   *
-  *** 
- *****
-Sample Input 2 :
-4
-Sample Output 2 :
-    *
-   *** 
-  *****
- *******
- 
- /************************************************** SOLUTION *****************************************************************************/
- 
- #include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int N;
-    cin>>N;
-    int i=1;
-    int val=1;
+
+int main()
+{
+    int n;
+    cout<<"Enter The No."<<endl;
+    cin>>n;
+    int i,ln,ts,ms,s,mb,b;
+    ts=1+(n-1)*2;
+    i=1;    
+    while(i<=n)
+   {
+        
+      ln=1;
+      while(ln<=i)
+          
+    {
+          ms= 1+(ln-1)*2;
+          mb= (ts-ms)/2;
+          b=1;
+          
+          while(b<=mb)
+          {
+              cout<<" ";
+              b=b+1;                
+          }
+          
+          s=1;
+          while(s<=ms)
+          {   cout<<"*";
+              s=s+1;   
+          }   
+          
+          b=1;
+          while(b<=mb)
+          {
+              cout<<" ";
+              b=b+1;
+          }
+          
+    }
+      cout <<endl;
+      ln=ln+1;
     
-    while(i<=N){
-        int space=1;
-      
-	
-        while(space<=N-i){
-        		cout<<" ";
-        		space=space+1;
-			}
-		int j=0;
-        val=i;
-        while(j!= 2*i-1){
-            cout<<"*";
-            j=j+1;
-        }
-        space=1;
-        while(space<=N-i){
-        		cout<<" ";
-        		space=space+1;
-			}
-        cout<<endl;
-        i=i+1;  
-	}
+      i=i+1;
+   }
+    
+    
+    
+    
 }
